b97a77edfe88f5af3b37eab323301cdc20f630f9
kevinshen56714 <kshen64@gatech.edu>
2022-04-27 10:24:40 -0400

    Introduce Procedure class

---
+++ pmd/core/Procedure.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from io import TextIOWrapper
+
+
+class Procedure(ABC):
+
+    @abstractmethod
+    def write_input(self):
+        pass
+
+
+class Minimization(Procedure):
+
+    def __init__(self, min_style: str = 'cg', etol: float = 10**(-6), ftol: float = 10**(-8), maxiter: int = 10**5, maxeval: int = 10**7):
+        self._min_style = min_style
+        self._etol = etol
+        self._ftol = ftol
+        self._maxiter = maxiter
+        self._maxeval = maxeval
+
+    def write_input(self, f: TextIOWrapper):
+        f.write('### Minimization\n')
+        f.write('{:<15} {}\n'.format('min_style', self._min_style))
+        f.write('{:<15} {} {} {} {}\n'.format(
+            'minimize', self._etol, self._ftol, self._maxiter, self._maxeval))
+        f.write('{:<15} 0\n'.format('reset_timestep'))
+        f.write('\n')
+        f.write('\n')
