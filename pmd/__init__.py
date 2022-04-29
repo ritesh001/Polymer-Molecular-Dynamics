018f50f4e5513742d5e975cea752ffaf51b6ac6a
kevinshen56714 <kshen64@gatech.edu>
2022-04-29 01:05:04 -0400

    Add analysis module and add more docs

--- pmd/__init__.py
+++ pmd/__init__.py
@@ -1,3 +1,5 @@
 from pmd.core.Lammps import Lammps
 from pmd.core.Job import Job
+from pmd.core.System import System
 from pmd.core.Procedure import Minimization, Equilibration, TgMeasurement, NVT, NPT
+from pmd.postprocess import Analysis
