d7c51e7ea3bf0b623c476c2f363584748276d369
kevinshen56714 <kshen64@gatech.edu>
2022-05-18 00:13:11 -0400

    Add tests

---
+++ pmd/tests/conftest.py
@@ -0,0 +1,7 @@
+"""
+    Dummy conftest.py for myproject.
+
+    Read more about conftest.py under:
+    - https://docs.pytest.org/en/stable/fixture.html
+    - https://docs.pytest.org/en/stable/writing_plugins.html
+"""
