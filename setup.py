05510e963ccbe99dcdb0a102279f507d9fb93939
kevinshen56714 <kshen64@gatech.edu>
2022-05-19 13:51:02 -0700

    Falling back to traditional setuptools for now (not ready to publish)

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:
-        print("\n\nAn error occurred while building the project, "
-              "please ensure you have the most updated version of setuptools, "
-              "setuptools_scm and wheel with:\n"
-              "   pip install -U setuptools setuptools_scm wheel\n\n")
-        raise
+    setup(
+        name='pmd',
+        version='0.1.0',
+        author='Kuan-Hsuan Shen',
+        author_email='kshen64@gatech.edu',
+        description='Automated generation of LAMMPS data and input files for polymer molecular dynamics simulations',
+        keywords=['LAMMPS', 'polymer', 'SMILES'],
+        url='https://github.com/Ramprasad-Group/High-throughput-MD-simulations',
+        packages=find_packages(),
+        python_requires=">=3.7",
+        entry_points={
+            "console_scripts": [
+                "pmd-load = pmd.entry.load:main",
+                "pmd-analyze = pmd.entry.analyze:main",
+            ]
+        },
+        zip_safe=False)
+    # try:
+    #     setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    # except:
+    #     print("\n\nAn error occurred while building the project, "
+    #           "please ensure you have the most updated version of setuptools, "
+    #           "setuptools_scm and wheel with:\n"
+    #           "   pip install -U setuptools setuptools_scm wheel\n\n")
+    #     raise
