aabc03f1f5211b94ae4815b849813d89baf7e5e7
kevinshen56714 <kshen64@gatech.edu>
2022-06-02 11:46:30 -0400

    Add basic Job, Pmd tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,29 @@
 """
-    Dummy conftest.py for myproject.
+    conftest.py for pmd.
 
     Read more about conftest.py under:
     - https://docs.pytest.org/en/stable/fixture.html
     - https://docs.pytest.org/en/stable/writing_plugins.html
 """
+
+import os
+import shutil
+
+import pytest
+
+
+@pytest.fixture
+def data_path(tmp_path, request):
+    '''
+    Fixture responsible for searching a folder with the same name of test
+    module and, if available, moving all contents to a temporary directory so
+    tests can use them freely.
+    '''
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+    d = tmp_path / "data"
+
+    if os.path.isdir(test_dir):
+        shutil.copytree(test_dir, d)
+
+    return d
--- tests/test_lammps.py
+++ tests/test_lammps.py
@@ -1,28 +1,8 @@
-import os
-import shutil
-
 import pytest
 
 from pmd.core import (GAFF2, Equilibration, Lammps, Minimization,
                       MSDMeasurement, ShearDeformation, TensileDeformation,
                       TgMeasurement)
-
-
-@pytest.fixture
-def data_path(tmp_path, request):
-    '''
-    Fixture responsible for searching a folder with the same name of test
-    module and, if available, moving all contents to a temporary directory so
-    tests can use them freely.
-    '''
-    filename = request.module.__file__
-    test_dir, _ = os.path.splitext(filename)
-    d = tmp_path / "data"
-
-    if os.path.isdir(test_dir):
-        shutil.copytree(test_dir, d)
-
-    return d
 
 
 @pytest.mark.parametrize(
