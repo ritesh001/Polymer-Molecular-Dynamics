cfb05744f6b7e44f428bd24c37c605020a81b612
kevinshen56714 <kshen64@gatech.edu>
2022-06-05 12:56:40 -0400

    Make tests covering more functional form generation

--- tests/test_lammps.py
+++ tests/test_lammps.py
@@ -1,25 +1,25 @@
 import pytest
 
-from pmd.core import (NPT, NVT, PSP, Equilibration, Lammps, Minimization,
+from pmd.core import (EMC, NPT, NVT, PSP, Equilibration, Lammps, Minimization,
                       MSDMeasurement, ShearDeformation, TensileDeformation,
                       TgMeasurement)
 
 
 @pytest.mark.parametrize(
-    "procedures, lmp_input_fname",
+    "procedures, builder, lmp_input_fname",
     [
-        (Minimization(), 'lmp_Minimization.in'),
-        ([Minimization()], 'lmp_Minimization.in'),
+        (Minimization(), PSP('gaff2-gasteiger'), 'lmp_Minimization.in'),
+        ([Minimization()], PSP('gaff2-gasteiger'), 'lmp_Minimization.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163)
-        ], 'lmp_Equilibration.in'),
+        ], PSP('opls-lbcc'), 'lmp_Equilibration.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163),
             TgMeasurement(
                 Tinit=600, Tfinal=100, Tinterval=20, step_duration=1000000)
-        ], 'lmp_TgMeasurement.in'),
+        ], EMC('opls-aa'), 'lmp_TgMeasurement.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163),
@@ -29,7 +29,7 @@ from pmd.core import (NPT, NVT, PSP, Equilibration, Lammps, Minimization,
                            duration=200000000,
                            dump_image=True,
                            reset_timestep_before_run=True)
-        ], 'lmp_MSDMeasurement.in'),
+        ], EMC('pcff'), 'lmp_MSDMeasurement.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163),
@@ -38,7 +38,7 @@ from pmd.core import (NPT, NVT, PSP, Equilibration, Lammps, Minimization,
                                T=300,
                                P=1,
                                reset_timestep_before_run=True)
-        ], 'lmp_TensileDeformation.in'),
+        ], EMC('trappe'), 'lmp_TensileDeformation.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163),
@@ -46,23 +46,24 @@ from pmd.core import (NPT, NVT, PSP, Equilibration, Lammps, Minimization,
                              erate=10**-6,
                              T=300,
                              reset_timestep_before_run=True)
-        ], 'lmp_ShearDeformation.in'),
+        ], PSP('gaff2-gasteiger'), 'lmp_ShearDeformation.in'),
         ([
             Minimization(),
             Equilibration(Teq=300, Peq=1, Tmax=800, Pmax=49346.163),
             NPT(Tinit=300, Tfinal=300, Pinit=1, Pfinal=1, duration=10**7),
             NVT(Tinit=300, Tfinal=300, duration=10**8)
-        ], 'lmp_NPT_NVT.in'),
+        ], PSP('gaff2-gasteiger'), 'lmp_NPT_NVT.in'),
     ],
 )
-def test_lammps_write(data_path, tmp_path, procedures, lmp_input_fname):
+def test_lammps_write(data_path, tmp_path, procedures, builder,
+                      lmp_input_fname):
     d = tmp_path / "result"
     expected_output = data_path / lmp_input_fname
     actual_output = d / lmp_input_fname
 
     # procedures provided through the constructor
     lmp = Lammps(read_data_from='data.lmps',
-                 get_functional_form_from=PSP('gaff2-gasteiger'),
+                 get_functional_form_from=builder,
                  procedures=procedures,
                  lmp_input_fname=lmp_input_fname)
     lmp.write_lammps(d)
@@ -71,7 +72,7 @@ def test_lammps_write(data_path, tmp_path, procedures, lmp_input_fname):
 
     # procedures provided through the add_procedure function
     lmp = Lammps(read_data_from='data.lmps',
-                 get_functional_form_from=PSP('gaff2-gasteiger'),
+                 get_functional_form_from=builder,
                  lmp_input_fname=lmp_input_fname)
     lmp.add_procedure(procedures)
     lmp.write_lammps(d)
