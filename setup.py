3552c75dae291087630478f0fca223b8f010831b
Kuan-Hsuan (Kevin) Shen <kshen64@gatech.edu>
2022-06-05 16:32:18 -0400

    Move dependencies to trigger GitHub dependency graph (#24)
    * Move dependencies to trigger GitHub dependency graph
    
    * Fix dependencies
    
    * Fix extras_require

--- setup.py
+++ setup.py
@@ -2,7 +2,23 @@ from setuptools import setup
 
 if __name__ == '__main__':
     try:
-        setup(use_scm_version={'version_scheme': 'no-guess-dev'})
+        # Metadata goes in setup.cfg. These are here for GitHub's dependency
+        # graph and use_scm_version
+        setup(name='pmd',
+              install_requires=[
+                  'importlib-metadata',
+                  'pyyaml>=5.0',
+                  'numpy>=1.0',
+                  'pandas>=1.0',
+                  'matplotlib>=3.0',
+                  'scipy>=1.0',
+                  'rdkit-pypi>=2022.3.2',
+                  'emc-pypi>=1.0.0',
+              ],
+              extras_require={
+                  'testing': ['setuptools', 'pytest', 'pytest-cov'],
+              },
+              use_scm_version={'version_scheme': 'no-guess-dev'})
     except Exception:
         print('\n\nAn error occurred while building the project, '
               'please ensure you have the most updated version of '
